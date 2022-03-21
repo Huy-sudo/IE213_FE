d0dd669da7d084c48ae291fdb48af7978790c735
nhd1207 <19521376@gm.uit.edu.vn>
2022-03-21 15:42:38 +0700

    Added more styling on Login page

--- src/components/Main/Authentication/Login/LoginForm.js
+++ src/components/Main/Authentication/Login/LoginForm.js
@@ -1,28 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 import style from "./LoginForm.module.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 function LoginForm() {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    function loginHandler(email, password) {
+
+    }
   return (
     <div className={style.background}>
-      <h1>SIGN UP</h1>
-      <Form className={style.loginform}>
+      <h1>LOG IN</h1>
+      <Form className={style.loginform} onSubmit={loginHandler}>
         <Form.Group className={"mb-3"} controlId="formBasicEmail">
-          <Form.Label>Email address</Form.Label>
-          <Form.Control type="email" placeholder="Enter email" />
+          <Form.Label className={style["login-label"]}>Email address</Form.Label>
+          <Form.Control className={style["login-input"]} type="email" placeholder="Enter email" onChange={event => setEmail(event.target.value)} />
         </Form.Group>
-
         <Form.Group className="mb-3" controlId="formBasicPassword">
-          <Form.Label>Password</Form.Label>
-          <Form.Control type="password" placeholder="Password" />
+          <Form.Label className={style["login-label"]}>Password</Form.Label>
+          <Form.Control className={style["login-input"]} type="password" placeholder="Password" onChange={event => setPassword(event.target.value)}/>
         </Form.Group>
         <Form.Group className="mb-3" controlId="formBasicCheckbox">
-          <Form.Check type="checkbox" label="Check me out" />
+          <Form.Check type="checkbox" label="Remember password" />
         </Form.Group>
-        <Button variant="primary" type="submit">
-          Submit
+        <Button className={style["login-button"]} variant="primary" type="submit">
+          Login
         </Button>
       </Form>
     </div>
