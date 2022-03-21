d4c6e5462f824097780d43204c602ad5446b3e73
nhd1207 <19521376@gm.uit.edu.vn>
2022-03-21 14:29:52 +0700

    Create React App, Added Bootstrap Library, LoginForm Components

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import LoginForm from './components/Main/Authentication/Login/LoginForm';
+
+function App() {
+  return (
+    <div className="App">
+      <LoginForm />
+    </div>
+  );
+}
+
+export default App;
