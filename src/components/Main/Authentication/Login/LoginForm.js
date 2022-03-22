f0e2f7181701144003e3dc71c12b481582322da3
Huy Hà Thúc <hathuchuy321@gmail.com>
2022-03-22 19:40:41 +0700

    Revert "Almost finishing Login Page"
    This reverts commit 7b0106c2e6b3aacc7b3d0f2d82f20bb7753b44cb.

--- src/components/Main/Authentication/Login/LoginForm.js
+++ src/components/Main/Authentication/Login/LoginForm.js
@@ -2,61 +2,31 @@ import React, { useState } from "react";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 import style from "./LoginForm.module.css";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faEnvelope, faKey } from "@fortawesome/free-solid-svg-icons";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 function LoginForm() {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
 
-  function loginHandler(email, password) {}
+    function loginHandler(email, password) {
+
+    }
   return (
     <div className={style.background}>
       <h1>LOG IN</h1>
       <Form className={style.loginform} onSubmit={loginHandler}>
         <Form.Group className={"mb-3"} controlId="formBasicEmail">
-          <Form.Label className={style["login-label"]}>
-            Email address
-          </Form.Label>
-          <div className={style["input-container"]}>
-            <FontAwesomeIcon
-              className={style["email-icon"]}
-              icon={faEnvelope}
-              size="xl"
-            />
-            <Form.Control
-              className={style["login-input"]}
-              type="email"
-              placeholder="Enter email"
-              onChange={(event) => setEmail(event.target.value)}
-            />
-          </div>
+          <Form.Label className={style["login-label"]}>Email address</Form.Label>
+          <Form.Control className={style["login-input"]} type="email" placeholder="Enter email" onChange={event => setEmail(event.target.value)} />
         </Form.Group>
         <Form.Group className="mb-3" controlId="formBasicPassword">
           <Form.Label className={style["login-label"]}>Password</Form.Label>
-          <div className={style["input-container"]}>
-            <FontAwesomeIcon
-              className={style["email-icon"]}
-              icon={faKey}
-              size="xl"
-            />
-            <Form.Control
-              className={style["login-input"]}
-              type="password"
-              placeholder="Password"
-              onChange={(event) => setPassword(event.target.value)}
-            />
-          </div>
+          <Form.Control className={style["login-input"]} type="password" placeholder="Password" onChange={event => setPassword(event.target.value)}/>
         </Form.Group>
         <Form.Group className="mb-3" controlId="formBasicCheckbox">
           <Form.Check type="checkbox" label="Remember password" />
         </Form.Group>
-        <Button
-          className={style["login-button"]}
-          variant="primary"
-          type="submit"
-        >
+        <Button className={style["login-button"]} variant="primary" type="submit">
           Login
         </Button>
       </Form>
