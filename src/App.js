92b198620fc03bcc2ffa9f4360343e85f8fdb5a5
Hiệp Nguyễn Hoàng <19520536@gm.uit.edu.vn>
2022-04-02 09:31:14 +0700

    hiep update admin

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 import LoginForm from './scenes/Auth/Login/LoginForm';
 import Signup from './scenes/Auth/Signup/index'
 import Home from './scenes/Home/index'
-import Admin from './scenes/Admin/Main/Admin';
+import Admin from './scenes/Admin/';
 import CarTable from './scenes/Admin/CarTable';
 function App() {
   return (
--- src/scenes/Admin/index.js
+++ src/scenes/Admin/index.js
@@ -7,7 +7,7 @@ import {
     Routes,
     Route
 } from "react-router-dom";
-import LayoutAdmin from './Layout';
+import LayoutAdmin from './LayoutAdmin';
 
 
 export default function Admin() {
